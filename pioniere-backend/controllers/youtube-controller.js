812cac6717e2b740b207300fe9cbea310b40cbe1
CapitanFindusFI <am95.dev@gmail.com>
2020-10-19 12:19:38 +0200

    first apis setup is working

--- pioniere-backend/controllers/youtube-controller.js
+++ pioniere-backend/controllers/youtube-controller.js
@@ -1,11 +1,42 @@
-var YTAPIClient = require('../lib/youtube-api-client');
+var { google } = require('googleapis');
+const { redis } = require('googleapis/build/src/apis/redis');
 
 class YoutubeController {
-    constructor(){
-        this.client = new YTAPIClient();
+    constructor() {
+        this.client = google.youtube({
+            version: "v3",
+            auth: process.env.YOUTUBE_API_KEY
+        });
     }
 
+    _doSearch(query) {
+        return this.client.search.list({
+            part: "snippet",
+            type: "video",
+            maxResults: 50,
+            q: query
+        }).then(res => res.data)
+            .then(data => data.items)
+    }
 
+    getVideoStatistics(videoIDs) {
+        return this.client.videos.list({
+            part: "statistics",
+            id: videoIDs.join(',')
+        }).then(res => res.data)
+            .then(data => {
+                console.log(data)
+                return data
+            })
+    }
+
+    getFirstResultPage(search, expectedViewCount) {
+        return this._doSearch(search);
+    }
+
+    getResultsForIMG(expectedViewCount) {
+        return this._doSearch("IMG_");
+    }
 }
 
 module.exports = YoutubeController
--- pioniere-backend/routes/index.js
+++ pioniere-backend/routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var YTController = require('../controllers/youtube-controller')
+
+const ytController = new YTController();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.sendStatus(200)
 });
+
+/* GET videos with 'IMG_' */
+router.get('/img', async (req, res, next) => {
+  try {
+    const items = await ytController.getResultsForIMG()
+    res.send(items).status(200)
+  } catch (e) {
+    res.send(e).status(500)
+  }
+})
+
+/* GET videos with 'IMG_', retrieve IDs only */
+router.get('/img-id', async (req, res, next) => {
+  try {
+    const items = await ytController.getResultsForIMG()
+      .then(items => items.map(i => i.id.videoId))
+    res.send(items).status(200)
+  } catch (e) {
+    res.send(e).status(500)
+  }
+})
 
 module.exports = router;
